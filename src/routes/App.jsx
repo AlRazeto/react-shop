b70d506acfd5592fb1c78309246391a1216c661d
alejorazeto <alejorazeto@gmail.com>
2021-12-27 19:11:17 -0300

    todo frontend convertido

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,12 +5,16 @@ import EditAccout from '../container/EditAccout';
 import Layout from '../container/Layout';
 import Login from '../container/Login';
 import NewPassword from '../container/NewPassword';
+import Order from '../container/Order';
 import RecoveryPassword from '../container/RecoveryPassword';
 import SignIn from '../container/SignIn';
 import Home from '../pages/Home'
 import NotFound from '../pages/NotFound'
 import ToyStore from '../pages/ToysStore';
 import '../styles/global.css';
+import Orders from '../container/Orders';
+import ProductDetails from '../components/ProductDetails';
+import ShopingCart from '../components/ShopingCart';
 
 const App =  () => {
     return(
@@ -25,6 +29,10 @@ const App =  () => {
                 <Route exact path='/recovery-password' element={<RecoveryPassword/>}/>
                 <Route exact path='/edit-account' element={<EditAccout/>}/>
                 <Route exact path='/toy-store' element={<ToyStore/>}/>
+                <Route exact path='/my-orders' element={<Orders/>}/>
+                <Route exact path='/my-order' element={<Order/>}/>
+                <Route exact path='/product-details' element={<ProductDetails/>}/>
+                <Route exact path='/shopping-cart' element={<ShopingCart/>}/>
                 <Route path = '*' element={<NotFound/>}/>
             </Routes>
         </Layout>
