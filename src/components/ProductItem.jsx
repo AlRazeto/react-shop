e125e13be5fa8a90fa98625927921774b22692be
alejorazeto <alejorazeto@gmail.com>
2021-12-31 15:40:48 -0300

    cart (incompleto)

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,11 +1,13 @@
-import React,{useState} from 'react';
+import React,{useContext} from 'react';
 import '@styles/ProductItem.scss';
+import addToCartImg from '@icons/bt_add_to_cart.svg'
 
-import addToCart from '@icons/bt_add_to_cart.svg'
+import AppContext from '../context/AppContext';
 
 const ProductItem = ({product}) => {
-	const[cart, setCart]=useState([])
-	const handleCart=() => { setCart([]) }
+	const { addToCart }=useContext(AppContext)
+	const handleClick=(Item) => { addToCart(Item) }
+	
 	return (
 		<div className="ProductItem">
 			<img src={product.images[0] } alt={product.title} />
@@ -14,8 +16,8 @@ const ProductItem = ({product}) => {
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={handleCart}>
-					<img src={addToCart} alt="" />
+				<figure onClick={() => handleClick(product)}>
+					<img src={addToCartImg} alt="" />
 				</figure>
 			</div>
 		</div>
